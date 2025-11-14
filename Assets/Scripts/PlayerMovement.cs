f3e3e44a821bf634c7928d4da9ed0cd08f8d9bb0
Darkim <andrea.ciancaglioni31@gmail.com>
2025-11-14 09:42:06 +0100

    Finished player movement (except for diagonal not normalized), added normal maps and dynamic lighting

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class NewEmptyCSharpScript
+public class PlayerMovement : MonoBehaviour
 {
-    
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpeed = 2.0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveH, moveV);
+
+        // DEBUG: show velocity after it's applied
+        Debug.Log($"Rigidbody2D velocity: {rb.velocity}");
+    }
 }
